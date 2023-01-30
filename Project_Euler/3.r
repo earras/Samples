df51a82686b8f2589bdf60274b0848dc66c5747f
earras <erin.e.arras@gmail.com>
2023-01-30 13:18:27 -0500

    Project_four

--- Project_Euler/3.r
+++ Project_Euler/3.r
@@ -1,17 +1,18 @@
 
 largest_prime_factor <- function(a) {
   for (x in rev(1:round(a/2))){
-    if (a %% x == 0){
-        for (y in x:round(x/2)) {
-            if (x %% y == 0) {
-                next
-            }
-        print(y)
+    if (a %% x ==0){
+      if (any (x %% 2:(x-1)==0)){
+        next
+      } else {
+        print (x)
         break
+      }
+  
+        
       }
       }
     }
-}
 
   
-largest_prime_factor(6851475143)
+largest_prime_factor(600851475143)
